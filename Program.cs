a399c74471a047b08f7e0eda70b00df0e409b384
PedroGomes15 <pedro.armando15@hotmail.com>
2017-05-28 23:46:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rpg_game_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
